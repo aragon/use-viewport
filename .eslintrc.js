9dd0d73ed23c5d51dbe399cf4959dbc70a0bfe34
Andy Hook <hello@andy-hook.co.uk>
2020-09-29 11:04:40 +0100

    Remove eslint dups, straight eslint eject via tsdx

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,11 @@ module.exports = {
   extends: [
     'react-app',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
-  plugins: ['react-hooks'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-  },
   settings: {
     react: {
-      pragma: 'React',
-      version: 'detect',
-    },
-  },
+      version: '999.999.999'
+    }
+  }
 }
