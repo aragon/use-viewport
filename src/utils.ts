ab6388fb7892994929265629988c90d1574db096
Andy Hook <hello@andy-hook.co.uk>
2020-10-01 17:53:39 +0100

    Rework errors to prevent attempted never in string

--- src/utils.ts
+++ src/utils.ts
@@ -14,26 +14,35 @@ export function withinBreakpointRange(
   max: string | number,
   viewportWidth: number
 ): boolean {
+  const minBreakpoint = BREAKPOINTS[min]
+  const maxBreakpoint = BREAKPOINTS[max]
+
   // Accept "" or -1 indifferently
   if (min === '') min = -1
   if (max === '') max = -1
 
   // Get the breakpoint unit value if the name is provided
-  if (typeof min === 'string') {
-    min = BREAKPOINTS[min]
+  if (minBreakpoint) {
+    min = minBreakpoint
+
+    if (typeof minBreakpoint !== 'number') {
+      throw new Error(`Viewport: invalid minimum value (${min}).`)
+    }
   }
-  if (typeof max === 'string') {
-    max = BREAKPOINTS[max]
+  if (maxBreakpoint) {
+    max = maxBreakpoint
+
+    if (typeof maxBreakpoint !== 'number') {
+      throw new Error(`Viewport: invalid maximum value (${max}).`)
+    }
   }
 
-  if (typeof min !== 'number') {
-    // eslint-disable-next-line
-    throw new Error(`Viewport: invalid minimum value (${min}).`)
+  // Throw when breakpoint name/s don't exist
+  if (typeof min === 'string' && !minBreakpoint) {
+    throw new Error(`Viewport: minimum breakpoint "${min}" doesn't exist.`)
   }
-
-  if (typeof max !== 'number') {
-    // eslint-disable-next-line
-    throw new Error(`Viewport: invalid maximum value (${max}).`)
+  if (typeof max === 'string' && !maxBreakpoint) {
+    throw new Error(`Viewport: maximum breakpoint "${max}" doesn't exist).`)
   }
 
   return (
